1bc34337bc41849d7173bea7e8c204716480465b
ForceScripts <71199348+IDKFORCE@users.noreply.github.com>
2023-04-06 14:07:05 -0700

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,51 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author '⸸♱♥IDK_FORCE♥♱⸸#7611'
+
+description 'fs-sounds'
+
+files {
+  'audio/sfx/resident/ambience.awc',
+  'audio/sfx/resident/animals_footsteps.awc',
+  'audio/sfx/resident/collision.awc',
+  'audio/sfx/resident/collisions.awc',
+  'audio/sfx/resident/doors.awc',
+  'audio/sfx/resident/explosions.awc',
+  'audio/sfx/resident/feet_materials.awc',
+  'audio/sfx/resident/feet_resident.awc',
+  'audio/sfx/resident/frontend.awc',
+  'audio/sfx/resident/low_latency.awc',
+  'audio/sfx/resident/melee.awc',
+  'audio/sfx/resident/movement.awc',
+  'audio/sfx/resident/player_switch.awc',
+  'audio/sfx/resident/vehicles.awc',
+  'audio/sfx/resident/weapons.awc',
+  'audio/sfx/resident/weather.awc',
+  'audio/sfx/weapons_player/lmg_combat.awc',
+  'audio/sfx/weapons_player/lmg_mg_player.awc',
+  'audio/sfx/weapons_player/mgn_sml_am83_vera.awc',
+  'audio/sfx/weapons_player/mgn_sml_am83_verb.awc',
+  'audio/sfx/weapons_player/mgn_sml_sc__l.awc',
+  'audio/sfx/weapons_player/ptl_50cal.awc',
+  'audio/sfx/weapons_player/ptl_50cal.oac',
+  'audio/sfx/weapons_player/ptl_combat.awc',
+  'audio/sfx/weapons_player/ptl_pistol.awc',
+  'audio/sfx/weapons_player/ptl_px4.awc',
+  'audio/sfx/weapons_player/ptl_rubber.awc',
+  'audio/sfx/weapons_player/sht_bullpup.awc',
+  'audio/sfx/weapons_player/sht_pump.awc',
+  'audio/sfx/weapons_player/smg_micro.awc',
+  'audio/sfx/weapons_player/smg_smg.awc',
+  'audio/sfx/weapons_player/snp_heavy.awc',
+  'audio/sfx/weapons_player/snp_rifle.awc',
+  'audio/sfx/weapons_player/spl_grenade_player.awc',
+  'audio/sfx/weapons_player/spl_minigun_player.awc',
+  'audio/sfx/weapons_player/spl_prog_ar_player.awc',
+  'audio/sfx/weapons_player/spl_railgun.awc',
+  'audio/sfx/weapons_player/spl_rpg_player.awc',
+  'audio/sfx/weapons_player/spl_tank_player.awc',
+}
+
+data_file 'AUDIO_WAVEPACK' 'audio/sfx/resident'
+data_file 'AUDIO_WAVEPACK' 'audio/sfx/weapons_player'
---
+++ readme.md
@@ -0,0 +1,15 @@
+# fs-sound
+
+FiveM Weapons & Other Sounds Made By (⸸♱♥IDK_FORCE♥♱⸸#7611)
+
+- ⚙️・Join My Discord And Open Ticket To Give Me Suggestions Or Report A Bug
+
+```My Discord```
+- [Discord](https://discord.gg/6kJ5ubDEWE)
+
+## Now If For Some Reason Some Times Is Client Side Only Is Weird I Gotta Mess With It Again
+
+
+# Only Bug As It Right Now That I Know Of
+
+```Lost ClientSide Audio```
